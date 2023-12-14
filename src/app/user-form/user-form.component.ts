7ac122493c3c5634a1d06965a7133e30b990d961
jimmy <jimmyhd_19@hotmail.com>
2023-12-13 21:43:22 -0500

    update user-form

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-user-form',
-  standalone: true,
-  templateUrl: './user-form.component.html',
-  styleUrl: './user-form.component.scss',
+	selector: 'app-user-form',
+	standalone: true,
+	templateUrl: './user-form.component.html',
+	styleUrl: './user-form.component.scss'
 })
 export class UserFormComponent {
-  inputMessage = 'Ingresa tus nombres';
-  disabledButton = false;
+	inputMessage = 'Ingresa tus nombres';
+	disabledButton = false;
 
-  clickSaveData(): void {
-    console.log('Guardando datos...');
-  }
+	input(event: Event): void {
+		const inputEvent = event.currentTarget as HTMLInputElement;
+		console.log('evento input...', inputEvent.value);
+	}
+
+	clickSaveData(): void {
+		console.log('Guardando datos...');
+	}
 }
