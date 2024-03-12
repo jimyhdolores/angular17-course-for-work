cb13366f39694ff9fee066dc8ecfa678cd51a8cf
jimmy <jimmyhd_19@hotmail.com>
2024-03-11 21:24:43 -0500

    final de la clase

--- src/app/services/demo.resolver.ts
+++ src/app/services/demo.resolver.ts
@@ -1,5 +1,5 @@
 import { ResolveFn } from '@angular/router';
 
 export const demoResolver: ResolveFn<boolean> = (route, state) => {
-  return true;
+	return true;
 };
--- src/app/services/products.resolver.ts
+++ src/app/services/products.resolver.ts
@@ -1,18 +1,29 @@
-import { Injectable, inject } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
-import { Observable } from 'rxjs';
+// import { Injectable, inject } from '@angular/core';
+// import { ActivatedRouteSnapshot, Resolve, ResolveFn } from '@angular/router';
+// import { Observable } from 'rxjs';
+// import { IApiResponseProduct } from './models/product-api. interface.';
+// import { ProductsApiService } from './products-api.service';
+
+// @Injectable({
+// 	providedIn: 'root'
+// })
+// export class ProductsResolverService implements Resolve<IApiResponseProduct[]> {
+// 	private readonly _productsApiService = inject(ProductsApiService);
+
+// 	resolve(route: ActivatedRouteSnapshot): Observable<IApiResponseProduct[]> {
+// 		console.log('Productos desde el resolver...', route);
+// 		// return this._productsApiService.getProducts().pipe(delay(3000));
+// 		return this._productsApiService.getProducts();
+// 	}
+// }
+
+import { inject } from '@angular/core';
+import { ResolveFn } from '@angular/router';
 import { IApiResponseProduct } from './models/product-api. interface.';
 import { ProductsApiService } from './products-api.service';
 
-@Injectable({
-	providedIn: 'root'
-})
-export class ProductsResolverService implements Resolve<IApiResponseProduct[]> {
-	private readonly _productsApiService = inject(ProductsApiService);
-
-	resolve(route: ActivatedRouteSnapshot): Observable<IApiResponseProduct[]> {
-		console.log('Productos desde el resolver...', route);
-		// return this._productsApiService.getProducts().pipe(delay(3000));
-		return this._productsApiService.getProducts();
-	}
-}
+export const ProductsResolverService: ResolveFn<IApiResponseProduct[]> = (route, state) => {
+	const _productsApiService = inject(ProductsApiService);
+	console.log('Productos desde el resolver...', route);
+	return _productsApiService.getProducts();
+};
